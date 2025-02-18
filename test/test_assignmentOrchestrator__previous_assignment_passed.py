cb511e5174f0b5051d3671c1b50bccd0989ae544
Mr. Python McPythony <PythonMcPythony@zerodaybootcamp.xyz>
2025-02-18 15:33:36 +0200

    added covverage for previous_Assignment_passed function in case user submits for assignment where the previous one doe snot exist

--- test/test_assignmentOrchestrator__previous_assignment_passed.py
+++ test/test_assignmentOrchestrator__previous_assignment_passed.py
@@ -38,9 +38,15 @@ def test_previous_assignment_passed___when_hacker_id_exists_and_previous_assignm
         "assignment_submission":AssignmentSubmission(hacker_id="existingId_1",assignment_id=2,assignment_file="myTask.tar.gz")
     }
     assert previous_assignment_passed(**tested_input) == False
-def test_previous_assignment_passed___when_hacker_id_exists_and_previous_assignment_failed():
+def test_previous_assignment_passed___when_hacker_id_exists_and_previous_assignment_passed():
     tested_input={
         "data":{"existingId_1":{"1":[{"result":"FAIL"},{"result":"FAIL"},{"result":"PASS"}]},"existingId_2":{}},
         "assignment_submission":AssignmentSubmission(hacker_id="existingId_1",assignment_id=2,assignment_file="myTask.tar.gz")
+    }
+    assert previous_assignment_passed(**tested_input) == True
+def test_previous_assignment_passed___when_hacker_id_exists_and_previous_assignment_does_not_exist():
+    tested_input={
+        "data":{"existingId_1":{"1":[{"result":"FAIL"},{"result":"FAIL"},{"result":"PASS"}]},"existingId_2":{}},
+        "assignment_submission":AssignmentSubmission(hacker_id="existingId_1",assignment_id=3,assignment_file="myTask.tar.gz")
     }
     assert previous_assignment_passed(**tested_input) == True
