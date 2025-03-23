149fdbabfdcedc31017718641b6ac41dfcc9fe05
Mr. Python McPythony <PythonMcPythony@zerodaybootcamp.xyz>
2025-03-23 13:05:41 +0200

    fixed redeploy script to leave data directory intact

--- redeploy.sh
+++ redeploy.sh
@@ -2,10 +2,12 @@
 service zeroDay stop
 cd /opt
 mkdir -p archivedDeployments
-tar -zcvf ./archivedDeployments/`date +%Y_%m_%d__%H_%M_%S`.tar.gz ./zeroDay/*
+archiveName=`date +%Y_%m_%d__%H_%M_%S.tar.gz`
+tar -zcvf ./archivedDeployments/${archiveName} ./zeroDay/*
 rm -rf ./zeroDay
 yum install git -y
 git clone https://github.com/python-ai-bootcamp/zeroDay.git
+tar -zxvf ./archivedDeployments/${archiveName} ./zeroDay/data
 cd ./zeroDay
 git remote set-url origin git@github.com:python-ai-bootcamp/zeroDay.git
 chmod 777 ./installService.sh ./tailLog.sh ./redeploy.sh ./renewCertificate.sh
