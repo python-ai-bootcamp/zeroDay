388db28ee61fe16b27101bab8d3532e30f4113ee
Mr. Python McPythony <PythonMcPythony@zerodaybootcamp.xyz>
2025-04-13 11:26:11 +0300

    added same log config yaml file command to startLocally script

--- startLocally.sh
+++ startLocally.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-start uvicorn --app-dir=src main:app --host 0.0.0.0 --port 8000
+start uvicorn --app-dir=src main:app --host 0.0.0.0 --port 8000 --log-config ./resources/config/logging.yaml
 start uvicorn --app-dir=src assignmentOrchestrator:app --host 0.0.0.0 --port 9000
