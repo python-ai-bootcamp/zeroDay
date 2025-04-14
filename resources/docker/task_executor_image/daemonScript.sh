de477d8e5312bfffe410a5344b975f29af552e81
Mr. Python McPythony <PythonMcPythony@zerodaybootcamp.xyz>
2025-04-14 18:43:01 +0300

    added empty file service for malijet domain authentication, and also fixed daemon script to be less verbose

--- resources/docker/task_executor_image/daemonScript.sh
+++ resources/docker/task_executor_image/daemonScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 exit_script() {
-    echo "recieved signal, setting continueWorking=false"
+    echo "`date`:: sandbox service docker daemon:: recieved SIGTERM or SIGINT signal, setting continueWorking='false', will exit in next iteration"
     continueWorking=false
 }
 
@@ -8,6 +8,6 @@ trap exit_script SIGINT SIGTERM
 continueWorking="true"
 while [ "${continueWorking}" == "true" ];
 do
-  echo "`date`:: continueWorking='${continueWorking}', so i'm continuing to work"
-  sleep 1;
+  echo "`date`:: sandbox service docker daemon:: continueWorking='${continueWorking}' -> not exiting"
+  sleep 5;
 done;
