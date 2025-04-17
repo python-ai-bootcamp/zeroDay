b011f79fd42bc9f6168d717d8fbc53e36eec4dee
Mr. Python McPythony <PythonMcPythony@zerodaybootcamp.xyz>
2025-04-17 15:37:57 +0300

    added assignments/current_State api

--- src/v2Apis.py
+++ src/v2Apis.py
@@ -1,4 +1,5 @@
 from systemEntities import AnalyticsEventType, print
+from assignmentOrchestrator import max_submission_for_assignment, next_assignment_submission
 from fastapi import APIRouter, Request
 
 router = APIRouter(prefix="/v2", tags=["v2"])
@@ -6,3 +7,13 @@ router = APIRouter(prefix="/v2", tags=["v2"])
 @router.get("/user")
 def serve_get_user(request: Request):
     return request.state.authenticated_user
+
+@router.get("/assignments/current_state")
+def serve_assignements_current_state(request: Request):
+    user=request.state.authenticated_user
+    if(user):
+        next_assignment_submission_status=next_assignment_submission(user["hacker_id"])
+        next_assignment_submission_status["max_submission_id"]=max_submission_for_assignment(next_assignment_submission_status["assignment_id"])
+        return next_assignment_submission_status
+    else:
+        return {"status":"ERROR", "ERROR_message":"user not found"}
