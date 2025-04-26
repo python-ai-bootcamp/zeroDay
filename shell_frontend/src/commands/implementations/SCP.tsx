f43600328e5782974f2a11b4a91fa9aa173546c7
micha <michavardy@gmail.com>
2025-04-26 09:03:53 +0300

    fixed scp, it works now :)

--- shell_frontend/src/commands/implementations/SCP.tsx
+++ shell_frontend/src/commands/implementations/SCP.tsx
@@ -7,25 +7,8 @@ export default function SCP({ args }: { args: string[]}) {
     const [scpContent, setScpContent] = useState<string>(''); // state to hold scp content
     const fileInputRef = useRef<HTMLInputElement>(null);
     const current_status_url=useApiUrl()("/v2/assignments/current_state")
-    const submit_assignment_url_template=useApiUrl()("/assignments/$${{ASSIGNMENT_ID}}$$/submission")
-    const handleDirectoryPick = async () => {
-      console.log("handleDirectoryPick:: entered")
-      try {
-        const dirHandle = await window.showDirectoryPicker();       
-        const files: File[] = [];
+    const submit_assignment_url_template=useApiUrl()("/v2/assignments/$${{ASSIGNMENT_ID}}$$/submission")
     
-        for await (const [name, handle] of dirHandle.entries()) {
-          if (handle.kind === 'file') {
-            const file = await handle.getFile();
-            file.webkitRelativePath = name; // optional: mimic relative path
-            files.push(file);
-          }
-        }
-        handleDirectoryUpload(files as any); // pass as FileList-like
-      } catch (err) {
-        console.error('User cancelled or error occurred:', err);
-      }
-    };
 
     const handleDirectoryUpload = async (files: FileList | null) => {
         if (!files) return;
@@ -42,7 +25,7 @@ export default function SCP({ args }: { args: string[]}) {
     
         // Upload the zip to your backend
         const formData = new FormData();
-        formData.append('file', blob, 'directory.zip');
+        formData.append('zip_file', blob, 'directory.zip');
     
         try {
           const user_status_res= await fetch(current_status_url)
@@ -82,7 +65,7 @@ export default function SCP({ args }: { args: string[]}) {
         webkitdirectory="true"
         directory=""
         multiple
-        onChange={(e) => handleDirectoryPick(e.target.files)}
+        onChange={(e) => handleDirectoryUpload(e.target.files)}
       />
     </div>
 
