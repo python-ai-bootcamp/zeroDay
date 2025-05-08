eb159ad8f4b9b2fa187e7598a38010cee9be49a7
Mr. Python McPythony <PythonMcPythony@zerodaybootcamp.xyz>
2025-05-08 23:50:56 +0300

    added production shell_frontend compiled files and a build script to create it easily

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#mkdir -p ./shell_frontend/dist/
+#rm -f ./shell_frontend/dist/main.tsx
+#cp -rp ./shell_frontend/src/main.tsx ./shell_frontend/dist/
+sed -i.bak 's/\(<AuthKeyProvider value="\).*\(">\)/\1\2/' ./shell_frontend/src/main.tsx 
+rm -f ./shell_frontend/dist/assets/index-*.css
+rm -f ./shell_frontend/dist/assets/index-*.js
+rm -f ./shell_frontend/dist/index.html
+rm -f ./resources/static/assets/index-*.css
+rm -f ./resources/static/assets/index-*.js
+rm -f ./resources/static/index.html
+
+pushd ./shell_frontend
+npm run build
+popd
+cp ./shell_frontend/dist/assets/index-*.css ./resources/static/assets/
+cp ./shell_frontend/dist/assets/index-*.js ./resources/static/assets/
+cp ./shell_frontend/dist/index.html ./resources/static/
+
+mv ./shell_frontend/src/main.tsx.bak ./shell_frontend/src/main.tsx
